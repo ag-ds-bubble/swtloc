0405f6d921355c7768c8dd2ecb73cf090cd4ed5d
Achintya Gupta <ag.ds.bubble@gmail.com>
2022-02-26 04:01:55 +0530

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -9,16 +9,13 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
-
-# -- Project information -----------------------------------------------------
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
 
 project = 'SWTLoc'
 copyright = '2022, Achintya'
@@ -46,7 +43,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
